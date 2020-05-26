4a4a1d54bbd33a2b0ee44834ee1916228ba62e78
Ali Klein <ali@riv.al>
2020-05-26 08:40:05 -0700

    auth context to home

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import app from "../base";
+import { AuthContext } from "../Auth";
+import Container from "../components/Container";
 import Button from "../components/Button";
 
 const Home = () => {
+  const { currentUser } = useContext(AuthContext);
+  console.log({ currentUser });
   return (
-    <>
+    <Container>
       <h1>Home</h1>
+      <p>Hello, {currentUser.email}</p>
       <Button text="Sign out" onClick={() => app.auth().signOut()} />
-    </>
+    </Container>
   );
 };
 
