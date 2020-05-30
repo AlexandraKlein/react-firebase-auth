66fb40a5a13fab1b4994466d89065ee2fbc9d6bc
Ali Klein <ali@riv.al>
2020-05-30 08:57:01 -0700

    read/write user info

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import * as firebase from "firebase/app";
+import "firebase/database";
+import { AuthContext } from "../Auth";
 import app from "../base";
 import Container from "../components/Container";
 import Button from "../components/Button";
@@ -12,7 +15,44 @@ const SignOutButton = styled(Button)`
   right: ${Gutters.MEDIUM};
 `;
 
+function writeUserData(user) {
+  firebase
+    .database()
+    .ref("users/" + user.uid)
+    .set(user)
+    .catch(error => {
+      console.log(error.message);
+    });
+}
+
+async function updateUserInfo(currentUser) {
+  try {
+    const user = {
+      phoneNumber: "4129530814",
+      address: "12345 Aroad Street",
+      uid: currentUser.uid,
+      email: currentUser.email,
+    };
+    writeUserData(user);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
 const Home = () => {
+  const { currentUser } = React.useContext(AuthContext);
+  const [userData, setUserData] = React.useState(undefined);
+
+  React.useEffect(() => {
+    firebase
+      .database()
+      .ref("users/" + currentUser.uid)
+      .once("value", snap => {
+        setUserData(snap.val());
+      });
+  }, [currentUser.uid]);
+
+  console.log(userData);
   return (
     <Container>
       <h1>Home</h1>
@@ -23,6 +63,7 @@ const Home = () => {
         text="Sign out"
         onClick={() => app.auth().signOut()}
       />
+      <Button text="Update Info" onClick={() => updateUserInfo(currentUser)} />
     </Container>
   );
 };
