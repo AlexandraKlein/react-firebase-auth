6a76a6de292c29387cd99931016f0cc4abc94228
Ali Klein <ali@riv.al>
2020-06-13 08:23:36 -0700

    post form styles

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import * as firebase from "firebase/app";
 import "firebase/database";
 import styled from "styled-components";
-import {
-  AiOutlineArrowDown,
-  AiOutlineArrowRight,
-  AiOutlineEdit,
-} from "react-icons/ai";
+import { AiOutlineArrowDown, AiOutlineEdit } from "react-icons/ai";
 import { AuthContext } from "../context/Auth";
 import { Row, Column } from "./Container";
 import { Heading } from "./Text";
 import ProfileImage from "./ProfileImage";
+import Form from "./Form";
 import { BreakPoint, Colors, Gutters } from "../styles";
 
-const postHeight = 160;
+const postHeight = 600;
+const postHeightMobile = 400;
 
 class PostForm extends React.PureComponent {
   state = {
@@ -47,7 +45,9 @@ class PostForm extends React.PureComponent {
         .ref("posts/" + Date.now())
         .set(post)
         .then(() => {
-          this.setState({ isUpdating: false, message: "" });
+          this.setState({ isUpdating: false, message: "" }, () => {
+            setTimeout(() => this.setState({ isOpen: false }), 1000);
+          });
         })
         .catch(error => {
           this.setState({ error: error.message });
@@ -62,20 +62,22 @@ class PostForm extends React.PureComponent {
     const { isOpen } = this.state;
 
     return (
-      <>
-        <StyledContainer
-          style={{ bottom: isOpen ? "0px" : `-${postHeight + 2}px` }}
+      <StyledContainer isOpen={isOpen}>
+        <ShowHide
+          align="flex-start"
+          onClick={() => this.setState({ isOpen: !isOpen })}
         >
-          <ShowHide
-            align="flex-start"
-            onClick={() => this.setState({ isOpen: !isOpen })}
+          <Heading marginTop="12px" marginBottom="0px" color={Colors.PRIMARY}>
+            {isOpen ? <AiOutlineArrowDown /> : <AiOutlineEdit />}
+          </Heading>
+        </ShowHide>
+        <StyledInnerContainer>
+          <StyledForm
+            submitText="Post"
+            isDisabled={this.state.message.length === 0}
+            onSubmit={this.writePostData}
           >
-            <Heading marginTop="12px" marginBottom="0px" color={Colors.PRIMARY}>
-              {isOpen ? <AiOutlineArrowDown /> : <AiOutlineEdit />}
-            </Heading>
-          </ShowHide>
-          <StyledInnerContainer>
-            <Row>
+            <Row justify="space-between">
               <ProfileImage
                 imgSrc={currentUser.photoURL}
                 marginBottom="0px"
@@ -87,24 +89,10 @@ class PostForm extends React.PureComponent {
                 onChange={this.onChange}
                 placeholder={`What's on your mind, ${currentUser.displayName}?`}
               ></StyledTextArea>
-              <Send
-                isDisabled={this.state.message.length === 0}
-                onClick={this.writePostData}
-              >
-                <Heading
-                  marginTop="0px"
-                  marginBottom="0px"
-                  color={Colors.PRIMARY}
-                  style={{ height: 28 }}
-                >
-                  <AiOutlineArrowRight />
-                </Heading>
-              </Send>
             </Row>
-          </StyledInnerContainer>
-        </StyledContainer>
-        <Gutter />
-      </>
+          </StyledForm>
+        </StyledInnerContainer>
+      </StyledContainer>
     );
   }
 }
@@ -120,10 +108,16 @@ export default DataProvidedPostForm;
 const StyledContainer = styled.div`
   display: flex;
   position: fixed;
-  height: ${postHeight}px;
+  height: ${postHeightMobile}px;
+  bottom:  ${props => (props.isOpen ? "0px" : `-${postHeightMobile + 2}px`)}
   width: 100%;
   border-top: 2px solid ${Colors.LIGHT_GRAY};
   transition: bottom 0.2s ease;
+
+  ${BreakPoint.TABLET}  {
+    height: ${postHeight}px;
+    bottom:  ${props => (props.isOpen ? "0px" : `-${postHeight + 2}px`)}
+  }
 `;
 
 const StyledInnerContainer = styled(Column)`
@@ -132,32 +126,35 @@ const StyledInnerContainer = styled(Column)`
   background-color: ${Colors.WHITE};
 `;
 
+const StyledForm = styled(Form)`
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  padding: ${Gutters.MEDIUM};
+
+  ${BreakPoint.TABLET} {
+    width: 600px;
+  }
+`;
+
+const StyledColumn = styled(Column)`
+  padding: ${Gutters.MEDIUM};
+
+  ${BreakPoint.TABLET} {
+    width: 600px;
+  }
+`;
+
 const StyledTextArea = styled.textarea`
+  flex: 1;
   background-color: ${Colors.INPUT};
   font-size: 16px;
   border: none;
   outline: 0;
   resize: none;
   padding: ${Gutters.MEDIUM};
-  width: 50%;
-  height: 80px;
-  margin: 0 ${Gutters.MEDIUM};
-
-  ${BreakPoint.TABLET} {
-    width: 400px;
-  }
-`;
-
-const Send = styled.div`
-  cursor: pointer;
-  pointer-events: ${props => (props.isDisabled ? "none" : "all")};
-  opacity: ${props => (props.isDisabled ? ".5" : "1")};
-
-  &:hover {
-    h2 {
-      color: ${Colors.PRIMARY_HOVER};
-    }
-  }
+  height: 100px;
+  margin-left: ${Gutters.MEDIUM};
 `;
 
 const ShowHide = styled(Row)`
@@ -180,8 +177,4 @@ const ShowHide = styled(Row)`
       color: ${Colors.PRIMARY_HOVER};
     }
   }
-`;
-
-const Gutter = styled.div`
-  height: ${postHeight}px;
 `;
