d40d7ef91e78175263a1fd0e02c73f15e6b83712
Ali Klein <alexandraeklein@yahoo.com>
2020-06-27 19:03:37 -0700

    infinite scroll

--- src/context/Posts.tsx
+++ src/context/Posts.tsx
@@ -13,10 +13,13 @@ export type PostType = {
 export type PostsContext = {
   posts: PostType;
   pending?: boolean;
+  referenceToOldestKey?: string;
 };
 
 const { Consumer, Provider } = React.createContext({
   posts: null,
+  pending: true,
+  referenceToOldestKey: "",
 });
 
 export { Consumer as PostsConsumer };
@@ -25,25 +28,78 @@ class PostsProvider extends React.Component<{}, PostsContext> {
   state = {
     posts: null,
     pending: true,
+    referenceToOldestKey: "",
   };
 
   componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
     this.fetchPosts();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
   fetchPosts = () => {
-    firebase
-      .database()
-      .ref("posts")
-      .orderByKey()
-      .once("value")
-      .then((snapshot) => {
-        this.setState({
-          posts: snapshot.val(),
-          pending: false,
-        });
-      })
-      .catch((error) => console.warn({ error }));
+    if (!this.state.referenceToOldestKey) {
+      firebase
+        .database()
+        .ref("posts")
+        .orderByKey()
+        .limitToLast(5)
+        .once("value")
+        .then((snapshot) => {
+          const arrayOfKeys = Object.keys(snapshot.val()).sort().reverse();
+          this.setState({
+            posts: snapshot.val(),
+            pending: false,
+            referenceToOldestKey: arrayOfKeys[arrayOfKeys.length - 1],
+          });
+        })
+        .catch((error) => console.error({ error }));
+    } else {
+      firebase
+        .database()
+        .ref("posts")
+        .orderByKey()
+        .endAt(this.state.referenceToOldestKey)
+        .limitToLast(6)
+        .once("value")
+        .then((snapshot) => {
+          const arrayOfKeys = Object.keys(snapshot.val())
+            .sort()
+            .reverse()
+            .slice(1);
+          this.setState({
+            posts: snapshot.val(),
+            pending: false,
+            referenceToOldestKey: arrayOfKeys[arrayOfKeys.length - 1],
+          });
+          console.log(this.state);
+        })
+        .catch((error) => console.error({ error }));
+    }
+  };
+
+  handleScroll = () => {
+    const windowHeight =
+      "innerHeight" in window
+        ? window.innerHeight
+        : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+    const windowBottom = windowHeight + window.pageYOffset;
+    if (windowBottom >= docHeight) {
+      this.fetchPosts();
+      console.log("bottom");
+    }
   };
 
   render() {
